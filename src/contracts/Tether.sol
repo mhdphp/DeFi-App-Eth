eda3afd09906c8dddc0c3158343107d29b94b08f
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-28 19:39:22 +0300

    Creating the transfer() function

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -7,4 +7,32 @@ contract Tether {
     string public symbol = 'USDT';
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
+
+    event Transfer (
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    event Approval (
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    // transfer from owner to other address
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] > _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
 }
