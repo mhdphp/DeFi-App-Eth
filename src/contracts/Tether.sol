720bd9371bc9fc8f456018e4693308b5f37818ff
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-28 18:47:28 +0300

    Changing .gitignore and Tether.sol

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.7.0 <0.9.0;
 
 
 contract Tether {
-    string name = 'Tether';
-    string symbol = 'USDT';
-    uint256 totalSupply = 1000000000000000000000000; // 1 million tokens
+    string public name = 'Mock Tether Contract';
+    string public symbol = 'USDT';
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
 }
