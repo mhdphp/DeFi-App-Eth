f8cbc859bcc9768b649140c2bab9c38f3806150a
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-29 19:49:00 +0300

    New contracts RDW.sol, DecentralBank.sol, deploy contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,14 @@
-const Tether= artifacts.require('Tether');
+const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD')
+const DecentralBank = artifacts.require('DecentralBank');
+
 
 module.exports = async function (deployer){
+    // deploy Tether contract
     await deployer.deploy(Tether);
     //const tether = await Tether.deployed();
+    // deploy RDW contract
+    await deployer.deploy(RWD);
+    // deploy DecentralBank contract
+    await deployer.deploy(DecentralBank);
 }
