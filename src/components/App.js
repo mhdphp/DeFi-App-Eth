d2b18e8fec5a53b7184523ab2ebb0d92fcf5b324
Mihai Enuica <mihai.enuica@gmail.com>
2022-04-04 12:39:37 +0300

    Creating App.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    // our React code goes in here
+    render() {
+        return(
+            <div>
+                <h2> Sa Te Alerg </h2>
+            </div>
+        );
+    }
+}
+
+
+export default App;
