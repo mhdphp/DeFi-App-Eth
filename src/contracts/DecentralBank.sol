84b8c70de713a59a5871111725ebbce3953359b9
Mihai Enuica <mihai.enuica@gmail.com>
2022-04-12 18:52:35 +0300

    Slightly refactor to get rid of run time error

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,71 +1,77 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-import './Tether.sol';
 import './RWD.sol';
+import './Tether.sol';
 
 contract DecentralBank {
-    string public name = "Decentral Bank";
-    address public owner;
-    // define contract Tether and RWD objects
-    Tether public tether;
-    RWD public rwd;
+  string public name = 'Decentral Bank';
+  address public owner;
+  Tether public tether;
+  RWD public rwd;
 
-    address[] public stakers;
-    // keeping track of staking address and amounts
-    mapping(address => uint256) public stakingBalance;
-    mapping(address => bool) public hasStaked;
-    mapping(address => bool) public isStaking;
+  address[] public stakers;
 
-    // bring Tether and RWD contracts into DecentralBank contract
-    constructor(Tether _tether, RWD _rwd) {
-        tether = _tether;
-        rwd = _rwd;
-        owner = msg.sender;
-    }
+  mapping(address => uint) public stakingBalance;
+  mapping(address => bool) public hasStaked;
+  mapping(address => bool) public isStaking;
 
-    // staking function
-    // investors deposit a Mock Tether coin to the address of this contract
-    // DecentralBank contract
-    function depositTokens(uint256 _amount) public{
-        require(_amount >0, 'amount can not be 0');
-        // transfer Mock Tether coins to this contract address for staking
-        tether.transferFrom(msg.sender, address(this), _amount);
-        // update stakingBalanace
-        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount ;
-        // check if the sender has already staked (is in stakers array)
-        if(!hasStaked[msg.sender]) {
-            stakers.push(msg.sender);
-        }
-        isStaking[msg.sender] = true;
-        hasStaked[msg.sender] = true;
-    }
+constructor(RWD _rwd, Tether _tether) {
+    rwd = _rwd;
+    tether = _tether;
+    owner = msg.sender;
+}
 
-     // issue token rewards - RWD
-     // the staker deposit Mock Tether Token and as a reward receives the 1/9 * ammount but
-     // in Reward Token
+  // staking function   
+function depositTokens(uint _amount) public {
+
+  // require staking amount to be greater than zero
+    require(_amount > 0, 'amount cannot be 0');
+  
+  // Transfer tether tokens to this contract address for staking
+  tether.transferFrom(msg.sender, address(this), _amount);
+
+  // Update Staking Balance
+  stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+  if(!hasStaked[msg.sender]) {
+    stakers.push(msg.sender);
+  }
+
+  // Update Staking Balance
+    isStaking[msg.sender] = true;
+    hasStaked[msg.sender] = true;
+}
+
+  // unstake tokens
+  function unstakeTokens() public {
+    uint balance = stakingBalance[msg.sender];
+    // require the amount to be greater than zero
+    require(balance > 0, 'staking balance cannot be less than zero');
+
+    // transfer the tokens to the specified contract address from our bank
+    tether.transfer(msg.sender, balance);
+
+    // reset staking balance
+    stakingBalance[msg.sender] = 0;
+
+    // Update Staking Status
+    isStaking[msg.sender] = false;
+
+  }
+
+  // issue rewards
         function issueTokens() public {
-            // require that only the contract owner to be able to issue tokens
-            require(msg.sender == owner, 'caller must be the contract owner');
-            // loop to stakers array
-            for (uint i=0; i<stakers.length; i++){
-                address recipient = stakers[i];
-                // each staker receives 1/9 of its balance staked in RWD tokens
+            // Only owner can call this function
+            require(msg.sender == owner, 'caller must be the owner');
+
+            // issue tokens to all stakers
+            for (uint i=0; i<stakers.length; i++) {
+                address recipient = stakers[i]; 
                 uint balance = stakingBalance[recipient] / 9;
-                if (balance > 0){
-                    rwd.transfer(recipient, balance);
-                }
+                if(balance > 0) {
+                rwd.transfer(recipient, balance);
             }
-    }
-
-    // Unstake tokens
-    function unstakedTokens() public{
-        uint balance = stakingBalance[msg.sender];
-        require(balance > 0,'staking balance can not be less than zero');
-        // transfer the tokens to the specified contract address from decentral bank
-        tether.transfer(msg.sender, balance);
-        stakingBalance[msg.sender] = 0;
-        isStaking[msg.sender] = false;
-    }
-
+       }
+       }
 }
--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -1,65 +1,57 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-
 contract Tether {
-    string public name = 'Mock Tether Token';
-    string public symbol = 'USDT';
+    string  public name = "Mock Tether Token";
+    string  public symbol = "mUSDT";
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
-    uint8 public decimals = 18;
-    address public owner;
+    uint8   public decimals = 18;
 
-    event Transfer (
+    event Transfer(
         address indexed _from,
-        address indexed _to,
-        uint256 _value
+        address indexed _to, 
+        uint _value
     );
 
-    event Approval (
+    event Approval(
         address indexed _owner,
-        address indexed _spender,
-        uint256 _value
+        address indexed _spender, 
+        uint _value
     );
 
     mapping(address => uint256) public balanceOf;
-    // first address is the owner the second mapping are the list of addresses with
-    // their allowances to transfer
     mapping(address => mapping(address => uint256)) public allowance;
-
+    
     constructor() {
         balanceOf[msg.sender] = totalSupply;
-        owner = msg.sender;
     }
 
-    // transfer from owner to other address
     function transfer(address _to, uint256 _value) public returns (bool success) {
+        // require that the value is greater or equal for transfer
         require(balanceOf[msg.sender] >= _value);
+         // transfer the amount and subtract the balance
         balanceOf[msg.sender] -= _value;
+        // add the balance
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-    // approve by the owner to third party to spend (_spender) in his behalf the _value
-    function approve(address _spender, uint256 _value) public returns (bool success){
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    // transfer inter third parties
-    // initiate transfer by third party(msg.sender) to transfer funds _from (owner), _to (recipient)
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
-        // _from is the owner of the token account
-        // _ to is the recipient of the transfer
-        // msg.sender is the third party approved to make the transfer on behalf of the owner (_from)
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
-        balanceOf[_from] -= _value;
+        // add the balance for transferFrom
         balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
+        // subtract the balance for transferFrom
+        balanceOf[_from] -= _value;
+        allowance[_from][msg.sender]-= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
-
 }
